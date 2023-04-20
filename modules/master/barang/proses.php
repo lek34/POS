8f6a7b516a05af2c9259e445d1c619f7b4169577
lek34 <97287300+lek34@users.noreply.github.com>
2023-04-20 20:26:54 +0700

    Update proses.php

--- modules/master/barang/proses.php
+++ modules/master/barang/proses.php
@@ -7,9 +7,7 @@ require_once "../../../config/database.php";
 
 // fungsi untuk pengecekan status login user 
 // jika user belum login, alihkan ke halaman login dan tampilkan pesan = 1
-if (empty($_SESSION['username']) && empty($_SESSION['password'])){
-	echo "<meta http-equiv='refresh' content='0; url=login.php?alert=1'>";
-}
+require_once "../../../auth/cek.php";
 // jika user sudah login, maka jalankan perintah untuk insert, update, dan delete
     if ($_GET['act']=='insert') {
         if(isset($_POST['addnewbarang'])){
