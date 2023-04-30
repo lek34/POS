afd5fa36f0ec7e5b0a9217e42f24a50706cd267b
Winar Joko Alexander <winaralex417@gmail.com>
2023-04-30 22:24:21 +0700

    Update pos.sql

--- mysql/pos.sql
+++ mysql/pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2023 at 10:58 AM
+-- Generation Time: Apr 30, 2023 at 05:23 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
