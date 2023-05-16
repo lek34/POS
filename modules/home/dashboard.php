e3a3b781e069efd0944be1e0b5502ce7bc59d267
geoffreyfarrel <geoffreyfarrel67@gmail.com>
2023-05-16 10:47:05 +0800

    push

--- modules/home/dashboard.php
+++ modules/home/dashboard.php
@@ -88,6 +88,28 @@
           <!-- Left col -->
           <section class="col-lg-7 connectedSortable">
             <!-- Custom tabs (Charts with tabs)-->
+            <!-- AREA CHART -->
+			  <div class="card card-primary">
+				<div class="card-header">
+				  <h3 class="card-title">Area Chart</h3>
+  
+				  <div class="card-tools">
+					<button type="button" class="btn btn-tool" data-card-widget="collapse">
+					  <i class="fas fa-minus"></i>
+					</button>
+					<button type="button" class="btn btn-tool" data-card-widget="remove">
+					  <i class="fas fa-times"></i>
+					</button>
+				  </div>
+				</div>
+				<div class="card-body">
+				  <div class="chart">
+					<canvas id="areaChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+				  </div>
+				</div>
+				<!-- /.card-body -->
+			  </div>
+			  <!-- /.card -->
             <div class="card">
               <div class="card-header">
                 <h3 class="card-title">
